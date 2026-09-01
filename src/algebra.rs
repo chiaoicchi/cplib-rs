5098901a08eb47d1594f418a3e8291865b5eebcd
chiaoicchi <182973681+chiaoicchi@users.noreply.github.com>
2026-09-01 18:04:19 +0900

    feat: group trait

--- src/algebra.rs
+++ src/algebra.rs
@@ -29,3 +29,41 @@ impl<T: Clone, F: Fn(&T, &T) -> T> Monoid for FnMonoid<T, F> {
         (self.op)(a, b)
     }
 }
+
+/// A group trait
+///
+/// # Definition
+/// ## Group
+/// A triple `(Value, id, op, inv)` is called a group if it satisfies:
+/// - (associativity) `op(op(a, b), c) == op(a, op(b, c))` for all `a`, `b`, `c`.
+/// - (identity) `op(id(), a) == op(a, id()) == a` for all `a`.
+/// - (inverse) `op(a, inv(a)) == op(inv(a), a) == id()` for all `a`.
+pub trait Group {
+    type Value;
+    fn id(&self) -> Self::Value;
+    fn op(&self, a: &Self::Value, b: &Self::Value) -> Self::Value;
+    fn inv(&self, a: &Self::Value) -> Self::Value;
+}
+
+/// A group built from closures.
+///
+/// # Contract
+/// `(T, id, op, inv)` must form a group,
+/// i.e. `op` is associative, `id` is its identity and `inv` is inverse.
+pub struct FnGroup<T, F, G> {
+    pub id: T,
+    pub op: F,
+    pub inv: G,
+}
+impl<T: Clone, F: Fn(&T, &T) -> T, G: Fn(&T) -> T> Group for FnGroup<T, F, G> {
+    type Value = T;
+    fn id(&self) -> T {
+        self.id.clone()
+    }
+    fn op(&self, a: &T, b: &T) -> T {
+        (self.op)(a, b)
+    }
+    fn inv(&self, a: &T) -> T {
+        (self.inv)(a)
+    }
+}
